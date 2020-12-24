940cf027d9e9f07788efbda2772713daa353ccc5
Edwin Moquete <edwinmmoquete@gmail.com>
2020-12-23 22:13:36 -0400

    fingerprint: goodix: Don't probe if fpsensor != 2
    Following the same logic as the fpc driver
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: Anush02198 <Anush.4376@gmail.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/input/fingerprint/gf3208/gf_spi.c
+++ drivers/input/fingerprint/gf3208/gf_spi.c
@@ -799,6 +799,11 @@ static int __init gf_init(void)
 {
 	int status;
 
+	if (fpsensor != 2) {
+		pr_err("Macle gf_init failed as fpsensor = %d(2=goodix)\n", fpsensor);
+		return -1;
+	}
+
 	/* Claim our 256 reserved device numbers.  Then register a class
 	 * that will key udev/mdev to add/remove /dev nodes.  Last, register
 	 * the driver which manages those device numbers.
